d2208be613c94dc554a4915964a02adf522514e9
Ivan R. Judson <irjudson@gmail.com>
2015-02-28 21:33:31 -0800

    Adding helper to debug partials

---
+++ tests/unit/helpers/debug-writer-test.js
@@ -0,0 +1,12 @@
+import {
+  debugWriter
+} from '../../../helpers/debug-writer';
+import { module, test } from 'qunit';
+
+module('DebugWriterHelper');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = debugWriter(42);
+  assert.ok(result);
+});
