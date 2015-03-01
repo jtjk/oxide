c23f57a1e422e132fb3f372e408a0d3c89f87412
Ivan R. Judson <irjudson@gmail.com>
2015-02-28 21:33:15 -0800

    Adding helper to debug partials

---
+++ app/helpers/debug-writer.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export function debugWriter(input) {
+  console.log('Current Context');
+  console.log('====================');
+  console.log(this);
+ 
+  if (input) {
+    console.log('Value');
+    console.log('====================');
+    console.log(input);
+  }	
+  return input;
+}
+
+export default Ember.Handlebars.makeBoundHelper(debugWriter);
